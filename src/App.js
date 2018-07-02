3ce724c048989fb6783920195777c71bc5921d21
Alexander Shannon <alexs1305@googlemail.com>
2018-07-02 21:22:25 +0100

    disable buttons on guess

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import GameEngine from './GameEngine/GameEngine';
 
 function NumbersComponent(props) {
 
-  return <div>{(() => props.numbers.map(n => <p>{n}</p>))()}</div>
+  return <div>{(() => props.numbers.map(n => <span>{n},</span>))()}</div>
 }
 
 function Button(props) {
   return <div className="col">
-    <button className="btn small-margin-right" onClick={() => props.action()}>
+    <button disabled= {props.isDisabled} className="btn small-margin-right" onClick={() => props.action()}>
       {props.name}
     </button>
   </div>
@@ -65,9 +65,9 @@ class App extends Component {
         <Title />
         <NumbersComponent numbers={this.state.numbers} /> 
         <div className="row">
-          <Button action={this.guessLower} name="Lower" />
-          <Button action={this.nextNumber} name="Reveal" />
-          <Button action={this.guessHigher} name="Higher" />
+          <Button isDisabled={this.state.guess===2} action={this.guessLower} name="Lower" />
+          <Button isDisabled={this.state.guess===0} action={this.nextNumber} name="Reveal" />
+          <Button isDisabled={this.state.guess===1} action={this.guessHigher} name="Higher" />
           <Win message={this.state.winMessage} />
         </div>
       </div>
